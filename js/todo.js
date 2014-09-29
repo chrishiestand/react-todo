1f339dea30163071366952b153fde56be523eca2
Chris Hiestand <chiestand@salk.edu>
2014-09-29 10:45:41 -0700

    merging previous changes
    somehow these didn't make it into the previous commit

--- js/todo.js
+++ js/todo.js
@@ -5,18 +5,35 @@
 var todoApp = React.createClass({
 
   updateData: function(newValue, index) {
-    var data    = this.state.data;
-    data[index] = data[index].concat([newValue]);
+    var data      = this.state.data;
+    var tmp       = {}
+    tmp[newValue] = false
+    data[index]   = data[index].concat([ tmp ]);
     this.setState({data: data});
   },
-  getInitialState: function() {
-    return {data: {'work': [ 'work1', 'work2'], 'home' : ['home1', 'home2'] }};
+
+  updateItem: function(itemDone, list_item, list_index) {
+    var data      = this.state.data;
+    var list_dict = _.filter(data[list_index], function(dict) {
+      return _.keys(dict)[0] == list_item;
+    })[0];
+    var dict_index = _.indexOf(data[list_index], list_dict);
+    data[list_index][dict_index][list_item] = itemDone;
+    this.setState({data:data});
   },
+
+  getInitialState: function() {
+    return { data: {'work' : [ {'work1' : false}, {'work2' : false}, {'already done' : true }],
+                    'home' : [ {'home1' : false}, {'home2' : false} ] 
+                  }
+           };
+  },
+
   render: function() {
       return (
         <div className="todoApp">
           <h1>TODO Lists</h1>
-          <todoLists data={this.state.data} updateData={this.updateData} />
+          <todoLists data={this.state.data} updateData={this.updateData} updateItem={this.updateItem} />
         </div>
       );
   }
@@ -28,10 +45,11 @@ var todoLists = React.createClass({
     var todo_list_names = Object.keys(this.props.data);
     var props           = this.props
     var todo_lists      = todo_list_names.map(function (list) {
+      var todo_count = $.grep(props.data[list], function(obj, i) { return _.values(obj)[0] === false }).length;
       return (
         <div className="todoList" key={list}>
-          <h2 className="todoList">{list}<span className="badge">{props.data[list].length}</span></h2>
-          <todoList data={props.data[list]} index={list} updateList={props.updateData} />
+          <h2 className="todoList">{list}<span className="badge">{todo_count}</span></h2>
+          <todoList data={props.data[list]} index={list} updateList={props.updateData} updateItem={props.updateItem}/>
         </div>
       );
     });
@@ -41,7 +59,6 @@ var todoLists = React.createClass({
       </div>
     );
   }
-
 });
 
 var todoList = React.createClass({
@@ -53,7 +70,8 @@ var todoList = React.createClass({
       if (newValue == '') {  // Do not accept whitespace only
         return;
       }
-      if ($.inArray(newValue, this.props.data) != -1 ) { //Not found
+      var list_items = _.map(this.props.data, function(list_items) {return Object.keys(list_items)[0]})
+      if ($.inArray(newValue, list_items) != -1 ) { //Not found
         alert(newValue + ' is already in the list')
         this.refs.newitem.getDOMNode().value = '';
         return;
@@ -63,13 +81,15 @@ var todoList = React.createClass({
       return;
     }
   },
+
   render: function() {
-    var todo_items = this.props.data.map(function (text) {
+    var props = this.props
+    var todo_items = this.props.data.map(function (dict) {
       return (
-        <todoItem key={text} text={text} />
+        <todoItem key={Object.keys(dict)[0]} text={Object.keys(dict)[0]} done={dict[Object.keys(dict)[0]]} index={props.index} updateItem={props.updateItem} />
       );
     });
-  
+
     return (
       <div className="todoList">
         <ul>
@@ -84,21 +104,28 @@ var todoList = React.createClass({
 
 var todoItem = React.createClass({
 
-  todoDone : function(e) {
-    var node = this.getDOMNode();
+  handleItemUpdate : function(e) {
+    var node      = this.getDOMNode();
+    var todo_item = this.props.text;
+    var item_done = $(node).find('input[type="checkbox"]').prop('checked')
+    this.props.updateItem(item_done, todo_item, this.props.index);
 
-    if ($(node).find('input[type="checkbox"]').prop('checked')) {
-      $(node).css('text-decoration', 'line-through');
-    }
-    else {
-      $(node).css('text-decoration', '');
+  },
+
+  componentDidMount: function() {
+    if (this.props.done) {
+      var node     = this.getDOMNode();
+      var checkbox =  $(node).find('input[type="checkbox"]')
+      $(checkbox).prop('checked', true);
     }
   },
+
   render: function() {
+    var done = this.props.done
     return (
-      <li className="todoItem">
+      <li className={done ? "todoItem done" : "todoItem"}>
       {this.props.text}
-      <input type="checkbox" onClick={this.todoDone} />
+      <input type="checkbox" onClick={this.handleItemUpdate} />
       </li>
     );
   }
@@ -112,7 +139,6 @@ var todoForm = React.createClass({
       );
   }
 });
-
 
 React.renderComponent(
   <todoApp />,
